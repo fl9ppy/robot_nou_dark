ab25a7233ec0c2efbc1a23b1def6324fcbfce972
fl9ppy <91787965+fl9ppy@users.noreply.github.com>
2023-06-14 11:02:25 +0300

    control sasiu

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/driveLULU.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/driveLULU.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.drive;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -8,8 +9,16 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 @TeleOp
+@Config
 public class driveLULU extends LinearOpMode {
-    @Override
+
+    enum Mode {
+        TURBO,
+        PRECISION,
+        DRIVER_CONTROL
+    }
+
+    Mode currentMode = Mode.DRIVER_CONTROL;
     public void runOpMode() throws InterruptedException {
 
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
@@ -21,21 +30,53 @@ public class driveLULU extends LinearOpMode {
         if (isStopRequested()) return;
 
         while (opModeIsActive() && !isStopRequested()) {
-            drive.setWeightedDrivePower(
-                    new Pose2d(
-                            -gamepad1.left_stick_y,
-                            -gamepad1.left_stick_x,
-                            -gamepad1.right_stick_x
-                    )
-            );
+
+            switch (currentMode) {
+
+                case DRIVER_CONTROL:
+                    drive.setWeightedDrivePower(
+                            new Pose2d(
+                                    -gamepad1.left_stick_y/2.5,
+                                    -gamepad1.left_stick_x/2.5,
+                                    -gamepad1.right_stick_x/2.5
+                            )
+                    );
+
+                    if (gamepad1.right_trigger!=0) currentMode = Mode.TURBO;
+                    if (gamepad1.left_trigger!=0) currentMode = Mode.PRECISION;
+
+                    break;
+
+                case TURBO:
+                    drive.setWeightedDrivePower(
+                            new Pose2d(
+                                    -gamepad1.left_stick_y,
+                                    -gamepad1.left_stick_x,
+                                    -gamepad1.right_stick_x
+                            )
+                    );
+
+                    if (gamepad1.right_trigger==0) currentMode = Mode.DRIVER_CONTROL;
+
+                    break;
+
+                case PRECISION:
+                    drive.setWeightedDrivePower(
+                            new Pose2d(
+                                    -gamepad1.left_stick_y/4,
+                                    -gamepad1.left_stick_x/4,
+                                    -gamepad1.right_stick_x/4
+                            )
+                    );
+
+                    if(gamepad1.left_trigger==0) currentMode = Mode.DRIVER_CONTROL;
+
+                    break;
+            }
 
             drive.update();
 
-            Pose2d poseEstimate = drive.getPoseEstimate();
-
-            telemetry.addData("x", poseEstimate.getX());
-            telemetry.addData("y", poseEstimate.getY());
-            telemetry.addData("heading", poseEstimate.getHeading());
+            telemetry.addData("Mod sasiu: ", currentMode.toString());
             telemetry.update();
         }
     }
