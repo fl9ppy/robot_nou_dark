4a168c665826e8cd30fb824683e7ac53c9c39e20
fl9ppy <91787965+fl9ppy@users.noreply.github.com>
2023-06-29 02:23:35 +0300

    a

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DARK/RobotUtils.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DARK/RobotUtils.java
@@ -19,11 +19,13 @@
 
 package org.firstinspires.ftc.teamcode.DARK;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
+@Config
 public class RobotUtils {
     public DcMotor slider1;
     public DcMotor slider2;
