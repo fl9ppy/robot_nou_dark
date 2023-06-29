e3f64a342cf321b367dde7845854316da295f777
maria2600 <116677645+Zxeniku@users.noreply.github.com>
2023-06-29 06:38:27 +0300

    bag pula in meep meep

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,22 +1,50 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+import java.nio.file.attribute.PosixFileAttributes;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(600);
 
-        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
-                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+        DefaultBotBuilder defaultBotBuilder = new DefaultBotBuilder(meepMeep);
+        defaultBotBuilder.setConstraints(65, 65, Math.toRadians(180), Math.toRadians(180), 15);// Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+        RoadRunnerBotEntity myBot = defaultBotBuilder
                 .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(new Pose2d(-36, -66, Math.toRadians(90)))
-                                .lineToSplineHeading(new Pose2d(-36, -10, Math.toRadians(90)))
-                                .turn(Math.toRadians(-120))
-                                .lineToSplineHeading(new Pose2d(-7,-17, Math.toRadians(-40)))
+                                .splineToSplineHeading(new Pose2d(-24,-35, Math.toRadians(0)), Math.toRadians(0))
+                                .splineToSplineHeading(new Pose2d(-8,-23, Math.toRadians(0)), Math.toRadians(85))
+                                .waitSeconds(0.250)
+                                .turn(Math.toRadians(90))
+                                .splineToSplineHeading(new Pose2d(-24,-11, Math.toRadians(0)), Math.toRadians(180))
+                                .splineToConstantHeading(new Vector2d(-58,-12) ,Math.toRadians(180))
+                                .waitSeconds(Math.toRadians(0.250))
+
+                                .splineToLinearHeading(new Pose2d(-30,-17, Math.toRadians(-45)), Math.toRadians(-80))
+                                .waitSeconds(Math.toRadians(0.250))
+                                .lineToSplineHeading(new Pose2d(-58,-12, Math.toRadians(0)))
+
+                                .splineToLinearHeading(new Pose2d(-30,-17, Math.toRadians(-45)), Math.toRadians(-80))
+                                .waitSeconds(Math.toRadians(0.250))
+                                .lineToSplineHeading(new Pose2d(-58,-12, Math.toRadians(0)))
+
+                                .splineToLinearHeading(new Pose2d(-30,-17, Math.toRadians(-45)), Math.toRadians(-80))
+                                .waitSeconds(Math.toRadians(0.250))
+                                .lineToSplineHeading(new Pose2d(-58,-12, Math.toRadians(0)))
+
+                                .splineToLinearHeading(new Pose2d(-30,-17, Math.toRadians(-45)), Math.toRadians(-80))
+                                .waitSeconds(Math.toRadians(0.250))
+                                .lineToSplineHeading(new Pose2d(-58,-12, Math.toRadians(0)))
+
+                                .splineToLinearHeading(new Pose2d(-30,-17, Math.toRadians(-45)), Math.toRadians(-80))
+                                .waitSeconds(Math.toRadians(0.250))
+                                .lineToSplineHeading(new Pose2d(-58,-12, Math.toRadians(0)))
+
                                 .build()
                 );
 
